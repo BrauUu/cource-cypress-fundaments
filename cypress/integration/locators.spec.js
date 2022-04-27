64498832aa518de9788ccbf6b941725b19c835a8
Brayan Frank <brayanfrank.sm@gmail.com>
2022-04-27 08:44:54 -0300

    adding comments

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -11,7 +11,13 @@ describe('Lidando com Seletores', () => {
         
         cy.get(':nth-child(2) > :nth-child(1) > :nth-child(3) > input')
 
+        //Buscando a partir do :contains do pai
+
         cy.get('#tabelaUsuarios tr:contains(\'Doutorado\'):eq(0) td:eq(6) > input')
+
+        //Buscando a partir do irmão
+
+        cy.get('#tabelaUsuarios td:contains(\'Doutorado\'):eq(0) ~ td:eq(3) > input')
 
     })
 })
