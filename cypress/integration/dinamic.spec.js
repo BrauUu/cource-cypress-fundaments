6dc6fd2c532a656029f967c3447cf4b8013ca5d5
Brayan Frank <brayanfrank.sm@gmail.com>
2022-05-16 22:39:39 -0300

    coding dinamic tests and working with local time

---
+++ cypress/integration/dinamic.spec.js
@@ -0,0 +1,67 @@
+/// <reference types="cypress" />
+
+const meals = [
+    "Carne",
+    "Frango",
+    "Pizza",
+    "Vegetariano"
+]
+
+describe('Testes dinâmicos', () => {
+    beforeEach(() => {
+
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+    })
+
+    meals.forEach(meal => {
+        
+        it(`Cadastro com ${meal}`, () => {
+    
+            cy.get('#formNome')
+                .type('Nome')
+    
+            cy.get('[data-cy="dataSobrenome"]')
+                .type('Sobrenome')
+    
+            cy.get(`[name=formSexo][value=M]`)
+                .click()
+    
+            cy.xpath(`//label[contains(., '${meal}')]/preceding-sibling::input`)
+                .click()
+    
+            cy.get('[data-test="dataEscolaridade"]').select('Doutorado')
+    
+            cy.get('[data-testid="dataEsportes"]').select('Corrida')
+    
+            cy.get('#formCadastrar').click()
+
+            cy.get('#resultado').should('contain', 'Cadastrado')
+    
+        })
+    })
+
+    it.only(`Cadastro selecionando todas opções de comida (Each)`, () => {
+    
+        cy.get('#formNome')
+            .type('Nome')
+
+        cy.get('[data-cy="dataSobrenome"]')
+            .type('Sobrenome')
+
+        cy.get(`[name=formSexo][value=M]`)
+            .click()
+
+        cy.get(`[name="formComidaFavorita"]`).each(option => {
+            cy.wrap(option)
+                .click()
+        })
+
+        cy.get('[data-test="dataEscolaridade"]').select('Doutorado')
+
+        cy.get('[data-testid="dataEsportes"]').select('Corrida')
+
+        cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
+
+    })
+})
---
+++ cypress/integration/time.spec.js
@@ -0,0 +1,58 @@
+
+const meals = [
+    "Carne",
+    "Frango",
+    "Pizza",
+    "Vegetariano"
+]
+
+describe('Testes dinâmicos', () => {
+    beforeEach(() => {
+
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+    })
+
+    const date = new Date('2022-05-16T17:00:00')
+
+    context('Manipulando horário da máquina', () => {
+
+        it('Utilizando cy.clock() sem parâmetros', () => {
+            
+            cy.clock()
+    
+            cy.get('#buttonNow').click()
+    
+            cy.get('#resultado > span').should('contain', '31/12/1969')
+
+        })
+
+        it('Utilizando uma data como parãmetro para o cy.clock()', () => {
+
+            cy.clock(date.getTime())
+    
+            cy.get('#buttonNow').click()
+    
+            cy.get('#resultado > span').should('contain', '16/05/2022')
+
+        })
+
+        it.only('Uilizando cy.tick', () => {
+
+            cy.clock()
+
+            cy.tick(1000)
+
+            cy.get('#buttonTimePassed')
+                .click()
+
+            cy.get('#resultado > span')
+                .invoke('text')
+                .should('eq', '1000')
+                
+
+        })
+    })
+    
+
+})
